a239f420c4f42f257b72eeaf1f1c1bee9fab22a7
Ken Vu <nghiavdng108@outlook.com>
2018-10-03 20:44:06 +0700

    Add skillActivity

--- app/src/main/java/ken/com/swoosh/LeagueActivity.kt
+++ app/src/main/java/ken/com/swoosh/LeagueActivity.kt
@@ -1,12 +1,18 @@
 package ken.com.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View) {
+        startActivity(Intent(this, SkillActivity::class.java))
     }
 }
