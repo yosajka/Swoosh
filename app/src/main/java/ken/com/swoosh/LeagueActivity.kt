479b8b64e64fac157acb8827e73d4415313878d2
Ken Vu <nghiavdng108@outlook.com>
2018-10-03 20:10:39 +0700

    Add baseActivity

--- app/src/main/java/ken/com/swoosh/LeagueActivity.kt
+++ app/src/main/java/ken/com/swoosh/LeagueActivity.kt
@@ -3,7 +3,7 @@ package ken.com.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
